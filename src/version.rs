412cbd7c75b984735d2f6765abfcdac2dc2895eb
Jorge Filipe <70545066+joojf@users.noreply.github.com>
2026-02-05 18:18:41 -0300

    Make --copy work by default (#8)

--- src/version.rs
+++ src/version.rs
@@ -1,44 +1,13 @@
-#[cfg(all(
-    not(feature = "clipboard"),
-    not(feature = "strength"),
-    not(feature = "dev-seed")
-))]
+#[cfg(all(not(feature = "strength"), not(feature = "dev-seed")))]
 pub const SHORT: &str = env!("CARGO_PKG_VERSION");
 
-#[cfg(all(
-    feature = "clipboard",
-    not(feature = "strength"),
-    not(feature = "dev-seed")
-))]
-pub const SHORT: &str = concat!(env!("CARGO_PKG_VERSION"), " (features: clipboard)");
-
-#[cfg(all(
-    not(feature = "clipboard"),
-    feature = "strength",
-    not(feature = "dev-seed")
-))]
+#[cfg(all(feature = "strength", not(feature = "dev-seed")))]
 pub const SHORT: &str = concat!(env!("CARGO_PKG_VERSION"), " (features: strength)");
 
-#[cfg(all(
-    not(feature = "clipboard"),
-    not(feature = "strength"),
-    feature = "dev-seed"
-))]
+#[cfg(all(not(feature = "strength"), feature = "dev-seed"))]
 pub const SHORT: &str = concat!(env!("CARGO_PKG_VERSION"), " (features: dev-seed)");
 
-#[cfg(all(feature = "clipboard", feature = "strength", not(feature = "dev-seed")))]
-pub const SHORT: &str = concat!(env!("CARGO_PKG_VERSION"), " (features: clipboard,strength)");
-
-#[cfg(all(feature = "clipboard", not(feature = "strength"), feature = "dev-seed"))]
-pub const SHORT: &str = concat!(env!("CARGO_PKG_VERSION"), " (features: clipboard,dev-seed)");
-
-#[cfg(all(not(feature = "clipboard"), feature = "strength", feature = "dev-seed"))]
+#[cfg(all(feature = "strength", feature = "dev-seed"))]
 pub const SHORT: &str = concat!(env!("CARGO_PKG_VERSION"), " (features: strength,dev-seed)");
-
-#[cfg(all(feature = "clipboard", feature = "strength", feature = "dev-seed"))]
-pub const SHORT: &str = concat!(
-    env!("CARGO_PKG_VERSION"),
-    " (features: clipboard,strength,dev-seed)"
-);
 
 pub const LONG: &str = SHORT;
